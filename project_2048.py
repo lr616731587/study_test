e3ec7b13f660347852dd68528d09d23cb87abcb3
zuo <616731587@qq.com>
2019-04-11 14:46:47 +0800

    2048 核心算法1:4个方向移动并整合

--- project_2048.py
+++ project_2048.py
@@ -16,7 +16,7 @@ def sort_2048(li):
     new_list = [i for i in li if i != 0]
     #  count 返回元素的数量
     new_list += [0] * li.count(0)
-    li[:] = new_list[:]
+    li[:] = new_list
 
     # for item in range(len(li), -1, -1):
     #     if item == 0:
@@ -57,7 +57,7 @@ def pri_list(li):
             print(li[i][j], end=' ')
         print()
 
-def sort_merge(list):
+def left_move(list):
     """
     向左移动并合并
         [2, 0, 0, 2]，               4 0 0 0
@@ -68,15 +68,62 @@ def sort_merge(list):
     :return:
     """
     for i in range(len(list)):
-        #左移
-        # merge_2048(list[i])
+        merge_2048(list[i])
 
-        #右移
-        new_list = list[i][::-1]
+def right_move(list):
+    """
+    向右移动并整合
+    :param list:
+    :return:
+    """
+    for i in range(len(list)):
+        new_list = list[i][::-1]  # 2 2 2 4
         merge_2048(new_list)
         list[i][::-1] = new_list[:]
 
-    pri_list(list)
+def right_to_down(list):
+    """
+    向右移动并整合
+    :param list:
+    :return:
+    """
+    for i in range(len(list)):
+        new_list = list[::-1]  # 2 2 2 4
+        merge_2048(new_list)
+        list[::-1] = new_list[:]
+
+def up_move(li):
+    # for i in range(len(li)):
+    #     for c in range(len(li)): #  c列 0
+    #         for r in range(len(li) - 1 ): #  r行 0 1 2 3
+    #             if li[r][c] < li[r + 1][c]:
+    #                 li[r][c], li[r + 1][c] = li[r + 1][c], li[r][c]
+
+    l = []
+    for c in range(len(li)):
+        for r in range(len(li)):
+            l.append(li[r][c])
+        merge_2048(l)
+
+        for i in range(len(l)):
+            li[i][c] = l[i]
+        l.clear()
+
+def down_move(li):
+    """
+    向下移动并合并
+    :param li:
+    :return:
+    """
+    l = []
+    for c in range(len(li)):
+        for r in range(len(li)):
+            l.append(li[r][c])
+        right_to_down(l)
+
+        for i in range(len(l)):
+            li[i][c] = l[i]
+        l.clear()
 
 
 list01 = [
@@ -87,4 +134,14 @@ list01 = [
     ]
 
 
-sort_merge(list01)
+# right_move(list01)
+# pri_list(list01)
+# print('-----------')
+# left_move(list01)
+# pri_list(list01)
+# print('-----------')
+# down_move(list01)
+# pri_list(list01)
+# print('-----------')
+# up_move(list01)
+# pri_list(list01)
