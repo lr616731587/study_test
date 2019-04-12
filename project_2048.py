6efc0f83dad9e6a9f273cd2ef656d0442a10240c
zuo <616731587@qq.com>
2019-04-12 10:22:10 +0800

    2048 核心算法1:4个方向移动并整合

--- project_2048.py
+++ project_2048.py
@@ -57,40 +57,29 @@ def pri_list(li):
             print(li[i][j], end=' ')
         print()
 
-def left_move(list):
+def left_move(li):
     """
     向左移动并合并
         [2, 0, 0, 2]，               4 0 0 0
         [2, 2, 0, 0],                4 0 0 0
         [2, 0, 4, 4],                2 8 0 0
         [4, 0, 0, 2]                 4 2 0 0
-    :param list:
+    :param li:
     :return:
     """
-    for i in range(len(list)):
-        merge_2048(list[i])
+    for i in range(len(li)):
+        merge_2048(li[i])
 
-def right_move(list):
+def right_move(li):
     """
     向右移动并整合
-    :param list:
+    :param li:
     :return:
     """
-    for i in range(len(list)):
-        new_list = list[i][::-1]  # 2 2 2 4
+    for i in range(len(li)):
+        new_list = li[i][::-1]  # 2 2 2 4
         merge_2048(new_list)
-        list[i][::-1] = new_list[:]
-
-def right_to_down(list):
-    """
-    向右移动并整合
-    :param list:
-    :return:
-    """
-    for i in range(len(list)):
-        new_list = list[::-1]  # 2 2 2 4
-        merge_2048(new_list)
-        list[::-1] = new_list[:]
+        li[i][::-1] = new_list[:]
 
 def up_move(li):
     # for i in range(len(li)):
@@ -99,15 +88,14 @@ def up_move(li):
     #             if li[r][c] < li[r + 1][c]:
     #                 li[r][c], li[r + 1][c] = li[r + 1][c], li[r][c]
 
-    l = []
     for c in range(len(li)):
+        l = []
         for r in range(len(li)):
             l.append(li[r][c])
         merge_2048(l)
 
         for i in range(len(l)):
             li[i][c] = l[i]
-        l.clear()
 
 def down_move(li):
     """
@@ -115,22 +103,21 @@ def down_move(li):
     :param li:
     :return:
     """
-    l = []
     for c in range(len(li)):
-        for r in range(len(li)):
+        l = []
+        for r in range(len(li) - 1, -1, -1):
             l.append(li[r][c])
-        right_to_down(l)
+        merge_2048(l)
 
-        for i in range(len(l)):
-            li[i][c] = l[i]
-        l.clear()
+        for i in range(len(li) - 1, -1, -1):
+            li[i][c] = l[len(li)- i -1]
 
 
 list01 = [
         [2, 0, 0, 2],
         [2, 2, 0, 0],
         [2, 0, 4, 4],
-        [4, 0, 0, 2]
+        [4, 2, 0, 2]
     ]
 
 
@@ -140,8 +127,8 @@ list01 = [
 # left_move(list01)
 # pri_list(list01)
 # print('-----------')
-# down_move(list01)
+down_move(list01)
 # pri_list(list01)
 # print('-----------')
 # up_move(list01)
-# pri_list(list01)
+pri_list(list01)
