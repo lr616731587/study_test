61e4065b5b4a09bcc79d7f5df771bfd92d680051
lr616731587 <616731587@qq.com>
2019-04-26 15:36:38 +0800

    Update View.py

--- View.py
+++ View.py
@@ -1,4 +1,6 @@
+import os
 from projects_2048.Conterllor import Conterllor
+from projects_2048.Location import Direction
 
 
 class View():
@@ -6,58 +8,61 @@ class View():
 
     def __init__(self):
         self.map = [
-            [2, 2, 2, 2],
-            [4, 4, 4, 4],
-            [8, 8, 8, 8],
-            [16, 16, 16, 16]
+            [4, 16, 4, 2],
+            [2, 16, 8, 4],
+            [32, 128, 16, 8],
+            [64, 2, 32, 16]
         ]
         self.con = Conterllor()
+        self.list = []
 
     def main(self):
-
         self.con.random_num(self.map)
-        self.con.random_num(self.map)
-        self.print_map()
+        self.__print_generate_new_number()
         while True:
-            if self.con.random_num(self.map) == 0:
+            self.__select_menu_item()
+            if self.con.is_change:
+                self.__print_generate_new_number()
+            else:
+                self.__print_map()
+
+            if self.con.game_is_over(self.map):
                 self.con.count = 0
                 self.map = [
-                    [0, 0, 0, 0],
-                    [0, 0, 0, 0],
-                    [0, 0, 0, 0],
-                    [0, 0, 0, 0]
-                ]
-            self.select_menu_item()
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]
+                    ]
+                self.con.random_num(self.map)
+                self.__print_generate_new_number()
 
-    def select_menu_item(self):
+    def __select_menu_item(self):
         select = input('请输入操作“w、a、s、d”')
         if select == 'w':
-            self.con.up_move(self.map)
-            self.random_print()
+            self.con.move(Direction().up, self.map)
 
         if select == 'a':
-            self.con.left_move(self.map)
-            self.random_print()
+            self.con.move(Direction().left, self.map)
 
         if select == 's':
-            self.con.down_move(self.map)
-            self.random_print()
+            self.con.move(Direction().down, self.map)
 
         if select == 'd':
-            self.con.right_move(self.map)
-            self.random_print()
+            self.con.move(Direction().right, self.map)
 
-    def random_print(self):
+    def __print_generate_new_number(self):
         self.con.random_num(self.map)
-        self.print_map()
+        self.__print_map()
 
-    def print_map(self):
+    def __print_map(self):
         print("----------------")
+        os.system('clear')
         print('目前总成绩为：{}'.format(self.con.count))
         if View.max_count < self.con.count:
             View.max_count = self.con.count
         print('最高记录为：{}'.format(View.max_count))
         for r in range(len(self.map)):
             for c in range(len(self.map[r])):
-                print(self.map[r][c], end="   ")
+                print(self.map[r][c], end='\t')
             print()
